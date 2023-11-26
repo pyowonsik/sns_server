68d26d60b5945e75f7c73f79933d0d9ca79caf52
pyowonsik <qqrtyu@gmail.com>
2023-11-26 12:10:41 +0900

    feat : typeorm 연동

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [PostsModule],
+  imports: [PostsModule,TypeOrmModule.forRoot({
+    type:'postgres',
+    host:'127.0.0.1',
+    port:5432,
+    username: 'postgres',
+    password: 'postgres',
+    database: 'postgres',
+    entities: [],
+    synchronize: true,
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
